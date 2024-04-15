0fa68616510e81d9c2ba58c7fa944f90a015068b
AlanProgrammer93 <alanwebprogrammer@gmail.com>
2024-04-15 18:22:45 -0300

    estructura inicial

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import LoginScreen from './pages/Login'
+
+function App() {
+
+  return (
+    <div className='App'>
+      <LoginScreen />
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './styles.css'
+
+const LoginScreen = () => {
+  return (
+    <div>LoginScreen</div>
+  )
+}
+
+export default LoginScreen
