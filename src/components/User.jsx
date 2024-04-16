048b38feb73994d716f8f0271e5df6551ae72b13
AlanProgrammer93 <alanwebprogrammer@gmail.com>
2024-04-15 21:23:38 -0300

    formato de fecha

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -2,10 +2,23 @@ import React from 'react'
 import { FaUserCircle } from "react-icons/fa";
 
 const User = ({ user }) => {
+    const convertirFecha = (fechaString) => {
+        const meses = [
+            "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
+            "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"
+        ];
+    
+        const partes = fechaString.split("-");
+        const year = partes[0];
+        const month = parseInt(partes[1], 10);
+        const day = parseInt(partes[2], 10);
+    
+        return `${meses[month - 1]} ${day}, ${year}`;
+    }
     return (
         <tr>
             <td>{`${user.name} ${user.surnames}`}</td>
-            <td>{user.birthDate}</td>
+            <td>{convertirFecha(user.birthDate)}</td>
             <td>
                 {
                     user.photo ? (
